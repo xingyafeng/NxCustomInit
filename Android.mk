014d2e1acbce7042c7d39600227db80a0fd1b0ba
yafeng <xingyf@yunovo.cn>
2018-07-18 18:47:25 +0800

    init init.yunovo.rc module.
    Change-Id: Idaf2853b34c564891d5839fdb282b86a1565df82

---
+++ Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_REQUIRED_MODULES := init.rc
+LOCAL_MODULE := init.yunovo.rc
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+
+include $(BUILD_PREBUILT)
+
+.PHONY: $(LOCAL_MODULE)_INSERT
+$(LOCAL_MODULE)_INSERT : $(TARGET_ROOT_OUT)/init.rc
+	$(info "---- insert init.yunovo.rc ... " )
+	$(shell \
+		if [ -f $(TARGET_ROOT_OUT)/init.rc -a -z "`cat $(TARGET_ROOT_OUT)/init.rc | grep init.yunovo.rc | awk -F/ '{print $2}'`"  ];then \
+			sed -i -e '/import \/init.trace.rc/a\import /init.yunovo.rc' $(TARGET_ROOT_OUT)/init.rc; \
+		fi \
+	)
+
+$(LOCAL_INSTALLED_MODULE) : $(LOCAL_MODULE)_INSERT
